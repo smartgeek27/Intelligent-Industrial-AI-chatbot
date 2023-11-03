6497699ffcf51b9a4e6b644a07152379d1bf55fc
Rishi Dadhich <87424679+smartgeek27@users.noreply.github.com>
2023-11-03 14:44:15 -0500

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, jsonify, request
-import config
-import openai
-import aiapi
-import notebook
-
-prompt = 'hey man whoc is elon musk ?'
-print(notebook.customChatGPTAnswer(prompt))
